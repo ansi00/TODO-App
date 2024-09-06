875986c33edfd86c2866730c2c380258c144b1cc
Anas Abbas <anasabbasmalik88@gmail.com>
2024-09-06 15:32:48 +0500

    Setting up the router and styling the todo-categories component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+import { TodoComponent } from './todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : '', component : CategoryComponent},
+    {path : 'todo', component : TodoComponent},
+     
+];
